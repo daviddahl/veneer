c05ff54c18e7716d1b6ae4fc9b0b968cc9d8d67d
David Dahl <ddahl@nulltxt.se>
2016-02-19 12:52:02 -0600

    added a test to create an UL element

--- src/index.js
+++ src/index.js
@@ -7,27 +7,57 @@ var veneer = {
     }
   },
 
-  makeNode: function (templateId, data, parent) {
+  makeNode:
+  function (templateId, data, parent, clear=false, attachmentPoint='append') {
     // Use Typo to handle arg validation
     var qid = '#' + templateId;
     var tmpl = document.querySelector(qid);
     var klasses = Object.keys(data);
-    for (var prop in data) {
-      var klass = '.' + prop;
-      var node = tmpl.content.querySelector(klass);
-      // TODO: switch on nodetype
-      node.textContent = data[prop];
+    for (let prop in data) {
+      // TODO: will have to identify the type of DOM node
+      // this data is being used with as not all have textContent property
+      if (typeof prop == 'string') {
+        let klass = `.${prop}`;
+        let node = tmpl.content.querySelector(klass);
+        // TODO: switch on nodetype
+        node.textContent = data[prop];
+      } else {
+        console.error('Unsupported node type');
+      }
     }
     var clone = document.importNode(tmpl.content, true);
+    if (clear) {
+      this.clearNode(parent);
+    }
     // TODO: Check if parent is a function or DOM node
-    this.attachNode(parent, clone);
+    this.attachNode(parent, clone, attachmentPoint);
     return clone;
   },
 
-  attachNode: function (parent, child, attachmentFn) {
-    parent.appendChild(child);
+  attachNode: function (parent, child, attachmentPoint) {
+    switch (attachmentPoint) {
+      case 'append':
+        parent.appendChild(child);
+        break;
+      case 'prepend':
+        this.prepend(parent, child);
+        break;
+      default:
+        return;
+    }
   },
-  
+
+  prepend: function (parent, child) {
+    parent.insertBefore(child, parent.firstChild);
+  },
+
+  clearNode: function (node) {
+    // remove all existing child nodes
+    while (node.firstChild) {
+      node.removeChild(node.firstChild);
+    }
+  },
+
 };
 
 export { veneer as default };
--- test/index.js
+++ test/index.js
@@ -1,5 +1,4 @@
 // veneer tests
-
 import veneer from '../src/index.js';
 
 function testMakeNode() {
@@ -7,11 +6,24 @@ function testMakeNode() {
     username: 'deeznuts',
     bio: 'Born in a log cabin, world traveller, whisky nut'
   }
+
   var parent = document.querySelector('#app');
-  veneer.makeNode('user-data', userData, parent);
+  var node = veneer.makeNode('user-data', userData, parent);
+  console.info('node created: ', node);
+}
+
+function testMakeList() {
+  var myList = ['passport', 'whisky bottle', 'backpack', 'mint gum'];
+  var parent = document.querySelector('.my-list');
+  for (let idx in myList) {
+    veneer.makeNode('my-list-items', {'list-item': myList[idx]}, parent);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  veneer.checkEnv();
   testMakeNode();
-  alert('tests finished!');
+  testMakeList();
+  console.info('tests finished!');
 });
+
